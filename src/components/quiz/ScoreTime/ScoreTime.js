a985e5154fd4ff6b52946af62c37d556f41ac585
Rajolia <Shivansh_Rajolia@intuit.com>
2020-01-04 09:07:12 +0530

    testing PR

--- src/components/quiz/ScoreTime/ScoreTime.js
+++ src/components/quiz/ScoreTime/ScoreTime.js
@@ -4,7 +4,7 @@ import {View, Text} from 'react-native';
 const ScoreTime = (props)=> {
     return (
         <View>
-            <Text>This is the score card component</Text>
+            <Text>This is the score card component. </Text>
         </View>
     )
 }
