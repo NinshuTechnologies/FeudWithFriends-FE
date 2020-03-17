05018b2aa3f630874237716fd41d93b0ecc5a4d5
Rajolia <Shivansh_Rajolia@intuit.com>
2020-03-17 18:51:39 +0530

    Question and Answer AOI Integration + handleAnswerSubmit

--- src/pages/SoloMode/SoloMode.js
+++ src/pages/SoloMode/SoloMode.js
@@ -1,8 +1,16 @@
-
-import React, {Component} from 'react';
-import {View, Text, SafeAreaView, KeyboardAvoidingView} from 'react-native';
+import React, {
+	Component
+} from 'react';
+import {
+	View,
+	Text,
+	SafeAreaView,
+	KeyboardAvoidingView
+} from 'react-native';
 import ScoreTime from '../../components/quiz/ScoreTimeBar/ScoreTimeBar';
-import { AnswerType } from '../../utilities/eums/AnswerTypeEnum';
+import {
+	AnswerType
+} from '../../utilities/eums/AnswerTypeEnum';
 import AnswerBoard from '../../components/quiz/AnsweBoard/AnswerBoard';
 import Question from '../../components/quiz/Question/Question';
 import styles from './SoloMode.css';
@@ -10,119 +18,116 @@ import AnswerInput from '../../components/quiz/AnswerInput/AnswerInput';
 import LinearGradient from 'react-native-linear-gradient';
 import axios from 'axios';
 
-class SoloMode extends Component{
+class SoloMode extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            answerList: [
-                {
-                    id: 1,
-                    answer: "Traffic",
-                    answerType: AnswerType.PRIVILEGED,
-                    points: 150,
-                    isAnswered: false
-                },
-                {
-                    id: 2,
-                    answer: "Medical Emergency",
-                    answerType: AnswerType.PRIVILEGED,
-                    points: 100,
-                    isAnswered: true
-                },
-                {
-                    id: 3,
-                    answer: "Kids",
-                    answerType: AnswerType.PRIVILEGED,
-                    points: 80,
-                    isAnswered: false
-                },
-                {
-                    id: 4,
-                    answer: "Bad Weather",
-                    answerType: AnswerType.PRIVILEGED,
-                    points: 50,
-                    isAnswered: false
-                },
-                {
-                    id: 5,
-                    answer: "Overslept",
-                    answerType: AnswerType.PRIVILEGED,
-                    points: 30,
-                    isAnswered: false
-                },
-                {
-                    id: 6,
-                    answer: "Alarm didnt work",
-                    answerType: AnswerType.PRIVILEGED,
-                    points: 15,
-                    isAnswered: true
-                },
-            ],
-            questionObj: {
-                id: null,
-                question: "null"
-            },
-            score: 0,
-            round: 1,
-            scoreMultiplier: 100,
-            userDataObj: {
-                firstName: null,
-                lastName: null,
-                experience: null,
-                isUserOnline: null,
-                userLevel: null,
-                coins: null,
-            }
-        };
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			answerList: [],
+			questionObj: {
+				id: null,
+				question: "null"
+			},
+			score: 0,
+			round: 1,
+			scoreMultiplier: 100,
+			strikes:0,
+			userDataObj: {
+				firstName: null,
+				lastName: null,
+				experience: null,
+				isUserOnline: null,
+				userLevel: null,
+				coins: null,
+			}
+		};
+	}
 
-    getQuestionAndAnswers = ()=> {
-        axios.get(`http://172.28.208.14:8080/question`)
-        .then(resp=> {
-            const { uid, question } = resp.data;
-            // axios.get(`http://192.168.29.247:8080/answer/questionId/${uid}`)
-            // .then(resp=> {
-            //     const {  }
-                this.setState({
-                    questionObj:{
-                        id: uid,
-                        question: question
+	getQuestionAndAnswers = () => {
+		axios.get(`http://192.168.29.247:8080/question`)
+			.then(resp => {
+				const {
+					uid,
+					question
+				} = resp.data;
+				axios.get(`http://192.168.29.247:8080/answer/questionId/${uid}`)
+					.then(resp => {
+						let answers = resp.data;
+						let answerObjForState = [];
+
+						answers.forEach((ans, index) => {
+							const {
+								uid,
+								displayAnswer,
+								rank
+							} = ans;
+
+							let answer = {
+								id: index + 1,
+								uid: uid,
+								rank: rank,
+								displayAnswer: displayAnswer,
+								points: Math.ceil(200/rank),
+								isAnswered: false
+							};
+							answerObjForState.push(answer);
+
+						});
+
+						this.setState({
+							questionObj: {
+								id: uid,
+								question: question
+							},
+							answerList: answerObjForState
+						});
+					})
+					.catch(e => {
+						console.log('failed in getting the answer ' + e);
+					})
+			})
+			.catch(e => {
+				alert('Error in fetching question.')
+			});
+	}
+
+	componentDidMount() {
+		if (!this.state.questionObj.id) this.getQuestionAndAnswers()
+	}
+
+
+	handleAnswerSubmit = (userAnswer) => {
+
+        let uri = `http://192.168.29.247:8080/answer/${this.state.questionObj.id}/${userAnswer}`;
+        // alert(uri);
+        axios.get(uri)
+			.then(resp => {
+                let actualDisplayAnswer = resp.data;
+                // alert(actualDisplayAnswer);
+                let indexToChange = null;
+                let answerListCopy = [...this.state.answerList];
+                let scoreGained=0;
+                answerListCopy.forEach((answer,index)=> {
+                    if(answer.displayAnswer == actualDisplayAnswer) {
+                        if(answer.isAnswered) {
+                            //write logic to display message that this answer is already guessed
+                            alert("Already guessed this one! Try another one.");
+                        } else {
+                            answer.isAnswered=true;
+                            scoreGained=answer.points
+                        }
                     }
                 });
-            // })
-            // .catch(e=> {
-            //     console.log('failed in getting the answer ' + e);
-            // })
-
-            // this.setState({
-            //     questionObj:{
-            //         id: uid,
-            //         question: question
-            //     }
-            // });
-        })
-        .catch(e=> {
-            alert('Error in fetching question.')
-        });
-    }
-
-    componentDidMount () {
-        if(!this.state.questionObj.id) this.getQuestionAndAnswers()
-    }
-
-
-    handleAnswerSubmit = (userAnswer)=> {
-
-        alert('inside the handler');
-        axios.get(`http://192.168.29.247:8080/answer/${this.state.questionObj.id}/${userAnswer}`)
-        .then(resp=> {
-            alert(resp.data);
-        })
-        .catch(e=> {
-            alert('Error in fetching answers.')
-        });
-    }
+                this.setState({
+                    answerList: answerListCopy,
+                    score: this.state.score + scoreGained
+                });
+                
+			})
+			.catch(e => {
+				alert('Wrong Answer!');
+			});
+	}
 
 
     render() {
@@ -141,7 +146,6 @@ class SoloMode extends Component{
 
                     <View style={{height: '25%', flexDirection: 'column', width: '100%', alignItems: 'center'}}>
                         <ScoreTime currentScore={this.state.currentScore}></ScoreTime>
-                        {/* <Text className={styles.Title}>FEUD WITH FRIENDS</Text> */}
                         {this.state.questionObj!==null &&
                             <Question questionObj={this.state.questionObj}/>
                         }   
